b1971341b0a53e6c75278e013be58ecb0e75b332
Tourduvalat <54987526+Tourduvalat@users.noreply.github.com>
2019-12-19 10:10:21 +0100

    Update script.js

--- sentinel-2/WIW/script.js
+++ sentinel-2/WIW/script.js
@@ -8,7 +8,4 @@
 
 return B8A<0.1804&&B12<0.1131?[51/255,68/255,170/255]:[B04*5,B03*5,B02*5]
 
-// return a blue color when water is detected in wetlands.
-// When no water, return lighten true color composition
-
-// False detection problems : on 
+// colorBlend will return a blue color when surface water is detected, and lighten to a natural color when no water is detected
